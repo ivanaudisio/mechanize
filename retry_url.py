da5faf6fcedd77f36b8ffe06083f7ae5c3c89405
ivanaudisio <ivan.audisio@gmail.com>
2017-04-29 19:49:18 +0100

    parameterized variables

--- retry_url.py
+++ retry_url.py
@@ -2,7 +2,9 @@ import re
 import time
 from mechanize import Browser
 
-max_retries=10
+max_retries=10 # maximum number of times to retry
+interval=5 # number of seconds to wait between retries
+url="http://cjoc:8080/job/shared-agent-01" # URL to open
 br = Browser()
 br.set_handle_robots(False)
 tried=0
@@ -10,13 +12,13 @@ connected = False
 
 while not connected:
     try:
-        response = br.open("http://cjoc:8080/job/shared-agent-01")
+        response = br.open(url)
         connected = True # if line above fails, this is never executed
     except:
         print "connection could not be establish"
-        time.sleep(5)
+        time.sleep(interval)
         tried += 1
     if tried > max_retries:
         exit()
 
-print "URL active"
+print "URL opened successfully"
