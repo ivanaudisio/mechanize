ba5fd23f222fe03757c6fa0ba0de5dd6d818abd2
ivanaudisio <ivan.audisio@gmail.com>
2017-04-29 19:41:12 +0100

    added timeout on number of tries

--- retry_url.py
+++ retry_url.py
@@ -2,12 +2,21 @@ import re
 import time
 from mechanize import Browser
 
+max_retries=10
+br = Browser()
+br.set_handle_robots(False)
 tried=0
 connected = False
+
 while not connected:
     try:
-        r = b.open('http://www.google.com/foobar')
-        connected = true # if line above fails, this is never executed
+        response = br.open("http://cjoc:8080/job/shared-agent-01")
+        connected = True # if line above fails, this is never executed
     except:
-        print "connection interrupted"
-        time.sleep(30)
+        print "connection could not be establish"
+        time.sleep(5)
+        tried += 1
+    if tried > max_retries:
+        exit()
+
+print "URL active"
